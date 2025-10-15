8dc6362a277f7162ea86992024162cc0ae2cdad6
aryandutt <aryandutt21@gmail.com>
2025-10-14 22:49:10 -0400

    adds stories section to home

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,14 @@
-
 "use client";
 
 import { Hero } from "./(home)/sections/Hero";
+import { StoriesCarousel } from "./(home)/sections/StoriesCarousel";
 
 export default function Home() {
   
   return (
     <div className="">
       <Hero lang="en" />
+      <StoriesCarousel lang="en" />
     </div>
   );
 }
