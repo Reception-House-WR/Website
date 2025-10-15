04fc55a0f6802833f134a94144ef085de7ea4f62
camiilavillamizar <camiilavillamizar@gmail.com>
2025-10-15 13:56:24 -0400

    Build working

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
           <ClientToolbar />
         {children}
         <DonateButton lang="en" />
+        <Footer/>
       </body>
-      <Footer/>
     </html>
   );
 }
