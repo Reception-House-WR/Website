87cc5d6d64c295eda83a4b074ab578eacca7c707
aryandutt <aryandutt21@gmail.com>
2025-10-15 00:35:45 -0400

    Upcoming Events section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Campaign } from "./(home)/sections/Campaign";
+import { EventsCalendar } from "./(home)/sections/EventCalender";
 import { Hero } from "./(home)/sections/Hero";
 import { StoriesCarousel } from "./(home)/sections/StoriesCarousel";
 
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero lang="en" />
       <StoriesCarousel lang="en" />
       <Campaign lang="en" />
+      <EventsCalendar lang="en" />
     </div>
   );
 }
