5d58f16e5304d1734f61cdadbe9e40648effbd1e
camiilavillamizar <camiilavillamizar@gmail.com>
2025-11-01 16:23:47 -0400

    Deleting dependencies

--- apps/web/src/app/programs-and-services/page.tsx
+++ apps/web/src/app/programs-and-services/page.tsx
@@ -1,11 +1,7 @@
 import Programs from "@/sections/programs-and-services/overview/programs";
 import Services from "@/sections/programs-and-services/overview/Services";
-import HealthcareSection from "@/sections/programs-and-services/health/Healthcare";
 import ServicesHero from "@/sections/programs-and-services/overview/ServicesHero";
-import { HousingSection } from "@/sections/programs-and-services/housing/HousingSection";
-import YouthSection from "@/sections/programs-and-services/children/YouthSection";
-import TrainingSection from "@/sections/programs-and-services/skills-training/TrainingSection";
-import FrenchServicesSection from "@/sections/programs-and-services/french/FrenchServices";
+
 
 export default function Home() {
   return (
