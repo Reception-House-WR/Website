d627986cebbce776afb48a9c8611da311f5622f0
aryandutt <aryandutt21@gmail.com>
2025-12-01 20:05:50 -0500

    adds more languages on the FE

--- apps/web/src/lib/strapi/helpers/localesHelper.ts
+++ apps/web/src/lib/strapi/helpers/localesHelper.ts
@@ -6,12 +6,21 @@ export type StrapiLocale = {
 
 export function fetchStrapiLocales(): StrapiLocale[] {
     return [
-        { code: "ar", name: "Arabic" },
-        { code: "en", name: "English", isDefault: true },
-        { code: "fr", name: "French" },
-        { code: "fa", name: "Persian" },
-        { code: "pt", name: "Portuguese" },
-        { code: "so", name: "Somali" },
-        { code: "es", name: "Spanish" },
+      { code: "am", name: "Amharic" },
+      { code: "ar", name: "Arabic" },
+      { code: "bn", name: "Bengali" },
+      { code: "en", name: "English", isDefault: true },
+      { code: "fr", name: "French" },
+      { code: "zh-Hans", name: "Mandarin" },
+      { code: "ne", name: "Nepali" },
+      { code: "ps", name: "Pashto" },
+      { code: "fa", name: "Persian" },
+      { code: "pt", name: "Portuguese" },
+      { code: "pa", name: "Punjabi" },
+      { code: "so", name: "Somali" },
+      { code: "es", name: "Spanish" },
+      { code: "sw", name: "Swahili" },
+      { code: "ti", name: "Tigrinya" },
+      { code: "ur", name: "Urdu" },
     ];
 }
